9439e7ac897531ce4a04c4eec77ace0e0b84dd6d
Jason Lapp <jason@vectorthis.com>
2014-12-07 10:18:55 -0500

    [#1] Show the user's location.

--- app/src/main/java/com/lappdance/grtrealtime/MapActivity.java
+++ app/src/main/java/com/lappdance/grtrealtime/MapActivity.java
@@ -10,7 +10,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity {
 
@@ -63,13 +62,11 @@ public class MapActivity extends FragmentActivity {
     }
 
     /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
+     * This is where we can add markers or lines, add listeners or move the camera.
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
     }
 
     /**
